cab0ffb8e6e2f6b24e303c3afefe61b3d03627bb
Theophile <theophile@iMac-de-administrador-3.local>
2018-08-27 15:02:03 +0200

    refactor code

--- src/index.js
+++ src/index.js
@@ -3,25 +3,23 @@ import {Component} from "react";
 
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import style from "./css/materialize.css";
-import style2 from "./css/style.css";
 
 
-class Button extends React.Component { 
-        constructor(props) {
-        super(props);
-        this.state = { counter: 1 };
-    }
+class Button extends React.Component {
 
 
-    render() { 
-   
-     return <div>
-        <h1>{this.state.counter}</h1>
-        <button onClick={() => {console.log('coucou')}}>go</button>   
-</div>
-    };
-
+    
+  
+  handleClick  ()  {
+  console.log('coucou')
+  };
+  
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        A
+      </button>
+    );
+  }
 }
-
 ReactDOM.render(<Button />, document.getElementById("index"));
